c932d4b66ea0b16b857d1bdc8687fa3326ed484b
Coco Yav <46410189+TangatheGoat@users.noreply.github.com>
2025-03-11 06:22:30 +0000

    Update Middleware.js

--- BackEnd/Lib/Middleware.js
+++ BackEnd/Lib/Middleware.js
@@ -1,11 +1,17 @@
 //author @james
-const Users = require("../Group11-main/Models/Users.Models");
+const Users = require("../Chomplog-main/Models/Users.Models");
 const loggedIn = function (req, res, next) {
   let XAuth = req.get("Authorization");
-  Users.getIdFromXAuth(XAuth, (err, id) => {
+ // Coco made a little change on author @James code
+  Users.getId(XAuth, (err, id) => {
     if (err || id == null) {
-      return res.statuse(401).send("id is null");
-    }
+      return res.status(401).send("Unauthorized");
+    }  // the end of change
+  
+   //Users.getIdFromXAuth(XAuth, (err, id) => {
+   // if (err || id == null) {
+    //  return res.statuse(401).send("id is null");
+    //}
     return id, next();
   });
 };
