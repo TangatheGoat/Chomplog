3d3d542d87d5b5730755e2b094ef9be80f708b08
Coco Yav <46410189+TangatheGoat@users.noreply.github.com>
2025-03-14 18:39:06 +0000

    Update database.js

--- BackEnd/database.js
+++ BackEnd/database.js
@@ -12,7 +12,7 @@ const db = new sqlite3.Database("./database.sqlite", (err) => {
     CREATE TABLE IF NOT EXISTS users (
     id INTEGER PRIMARY KEY AUTOINCREMENT, 
     username TEXT,
-    email TEXT UNIQUE2,
+    email TEXT UNIQUE,
     password TEXT,
     salt TEXT,
     created_at TEXT
