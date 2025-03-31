fcdc28e5b7f52f19db1bdfd036e1ff0676c91b5a
James Thorpe <jthorpe112@yahoo.com>
2025-03-31 14:18:25 +0100

    re-testing files

--- chomplog/src/router/index.js
+++ chomplog/src/router/index.js
@@ -1,9 +1,11 @@
 import { createRouter, createWebHistory } from 'vue-router'
 
 import Home from "../view/pages/Home.vue"
+import Login from '@/view/pages/Login.vue'
 
 const routes =[
     {path: "/", component: Home},
+    {path: "/login", component :Login}
 
 ]
 
