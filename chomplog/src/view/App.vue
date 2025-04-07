5001f71165c4ccbc6560d61b531bdc89dd1d3124
James Thorpe <jthorpe112@yahoo.com>
2025-04-07 14:34:50 +0100

    final implementation of pages
    I have completed that JavaScript and the implementation of all the front-end pages

--- chomplog/src/view/App.vue
+++ chomplog/src/view/App.vue
@@ -2,32 +2,37 @@
 </meta>
 <template>
   <header class="web-header">
-<div class="web-logo">
-<img src="../assests/logo.png" alt="Chomplog Logo">
-<h1>Chomplog</h1>
-</div>
+    <div class="web-logo">
+      <img src="../assests/logo.png" alt="Chomplog Logo">
+      <h1>Chomplog</h1>
+    </div>
 
-<button class="menu" @click="toggleMenu">
-  &#9776;
-</button>
-<nav class="nav" :class="{ 'nav-open': showMenu }">
-<ul>
-  <li><RouterLink to="/">Home</RouterLink></li>
-  <li><RouterLink to="/login">login</RouterLink></li>
-  <li><RouterLink to ="/register">register</RouterLink></li>
-</ul>
-</nav>
-</header>
-<RouterView/>
+    <button class="menu" @click="toggleMenu">
+      &#9776;
+    </button>
+    <nav class="nav" :class="{ 'nav-open': showMenu }">
+      <ul>
+        <li>
+          <RouterLink to="/">Home</RouterLink>
+        </li>
+        <li>
+          <RouterLink v-if="showLogin" to="/login">login</RouterLink>
+          <RouterLink v-if="showMain" to="/MainPage">User Page</RouterLink>
+        </li>
+        <li>
+          <RouterLink v-if="showLogin" to="/register">register</RouterLink>
+          <RouterLink v-if="showMain" to="/" v-on:click="logoutUser">logout</RouterLink>
+        </li>
+      </ul>
+    </nav>
+  </header>
+  <main>
+    <RouterView />
+  </main>
 
-  <div class="NHS-links">
-    <a href="https://www.nhs.uk/live-well/eat-well/food-guidelines-and-food-labels/the-eatwell-guide/" class="nhs-info-link">Balance diet</a>
-<a href="https://www.nhs.uk/better-health/lose-weight/calorie-counting/"class="nhs-info-link">  Understanding Calories</a>  
-<a href="https://www.nhsinform.scot/healthy-living/food-and-nutrition/healthy-eating-and-weight-management/tips-on-how-to-maintain-a-healthy-weight/"class="nhs-info-link">Weight maintainance </a>  
-</div>
-<footer class="web-footer">
-  <div class="web-footer-copyright">
-      <p>&copy; 2025 Chomplog All rights reserved.</p>
+  <footer class="web-footer">
+    <div class="web-footer-copyright">
+      <p>&copy; 2025 Chomplog All rights reserved.</p>s
     </div>
     <div class="web-footer-content">
       <div class="web-footer-section">
@@ -41,50 +46,90 @@
         <p>help@Chomplog.com</p>
       </div>
     </div>
-   
+
   </footer>
 
 </template>
 
 <script>
+import { UserService } from '../services/User.service';
 import { RouterLink } from 'vue-router';
+
+
+
 export default {
-  name: 'Header',
-  data(){
+  name: 'App',
+  data() {
     return {
-      showMenu: false
+      loginToken: localStorage.getItem('loginToken'),
+      showMenu: false,
+      showLogin: false,
+      showMain: false,
+      error: ""
+
+
     };
   },
   methods: {
-    toggleMenu(){
+    update: function () {
+      if (localStorage.getItem('loginToken')) {
+        this.showMain = true
+        this.showLogin = false
+      } else if (localStorage.getItem('loginToken') === null) {
+        this.showLogin = true
+        this.showMain = false
+      }
+    },
+    toggleMenu() {
       this.showMenu = !this.showMenu;
+    },
+    logoutUser() {
+      UserService.logout(this.session_token)
+        .then(logout => {
+          console.log("logged Out")
+          if (logout) {
+            window.location.reload()
+          }
+        })
+        .catch(error => this.error = error)
     }
+  },
+  created(){
+    this.update();
   }
-  }
-  </script>
-  
-  <style scoped>
+}
+</script>
+
+<style scoped>
+@media screen {
   .web-header {
     position: relative;
     display: flex;
     justify-content: space-between;
     align-items: center;
     padding: 0.25rem;
-    font-size: 0.8rem;
+    font-size: 0.6rem;
     color: white;
     background-color: #3D5315;
   }
+
   .web-logo {
     display: flex;
     align-items: center;
   }
+
   .web-logo img {
     border: #82a73d;
     border-radius: 20%;
-    width: 60px;
-    height: 60px;
+    width: 50px;
+    height: 50px;
     margin-right: 0.5rem;
-  padding-left: 1.25%;
+    padding-left: 1.25%;
+  }
+
+  .menu {
+    display: none;
+
   }
 
   .nav {
@@ -93,8 +138,9 @@ export default {
     justify-content: flex-end;
     padding-right: 10%;
   }
-  .nav ul{
-    font-size: large;
+
+  .nav ul {
+    font-size: 1rem;
     list-style: none;
     display: flex;
     gap: 6rem;
@@ -103,266 +149,59 @@ export default {
   }
 
   .nav a {
-  color: white;
-  text-decoration: none;
-  font-weight : bold;
-  }
-  .nav a:hover{
-  text-decoration: underline;
-  }
-  .menu{
-    display: none;
-  font-size: 1.5rem;
-  background: none;
-  cursor: pointer;
-  color: white;
-  position: absolute;
-  top: 0.5rem;
-  right: 0.5rem;
-  z-index: 1001;
-  }
- 
-.web-intro {
-  color: #3D5315;
-  font-size: 1.25rem;
-  font-weight: bold;
-  text-align: center;
-  margin-top: 1rem;
-padding-left: 0.75%
-
-}
-  .background-image {
-  position: relative;
-  margin-top: 1rem;
-  text-align: center;
-  height: 50vh;
-  align-items: center;
-  }
-  .background-image img {
-    width: 100%;
-    height: 100%;
-  }
-  .input-circle {
-   width: 13rem;
-    height: 13rem;
-    background-color: white;
-    border: 3px solid #3D5315 ;
-    border-radius: 50rem;
-    display: flex;
-    position: absolute;
-    top: 50%; 
-  left: 50%; 
-  transform: translate(-50%, -50%); 
- 
-    
-  }
-  
-  .NHS-links {
+    color: white;
     text-decoration: none;
-    display: flex;
-    justify-content: space-around;
-    color: #3D5315;
-    margin-top: 2.5rem;
-    margin-bottom: 3rem;
-   
+    font-weight: bold;
   }
-  .nhs-info-link{
-   text-decoration: underline;
-    color: #3D5315;
-    font-size: 1.3rem;
-    padding: 0.4rem;
+
+  .nav a:hover {
+    text-decoration: underline;
   }
-  .nhs-info-link:hover {
+
+
+  .web-footer {
     background-color: #3D5315;
     color: white;
+    padding: 1rem;
+    text-align: center;
+    margin-top: 1rem;
   }
-  .web-footer {
-  background-color: #3D5315; 
-  color: white; 
-  padding-top: 0.5rem;
-  padding-bottom: 0.5rem;
-  padding-left: 5rem;
-  padding-right: 5rem;
-  }
-.web-footer-content {
-  display: flex;
-  justify-content: space-between;
-}
 
-
-.web-footer-copyright {
-  text-align: center;
-  font-size: 1rem;
-}
-  .web-footer-section{
-    font-size:0.8rem;
-  }
-  @media (max-width: 768px) {
-    .web-header {
-    position: relative;
+  .web-footer-content {
     display: flex;
     justify-content: space-between;
-    align-items: center;
-    padding: 0.25rem;
-    font-size: 0.8rem;
-    color: white;
-    background-color: #3D5315;
   }
-    .web-header {
-      position: relative;
-      justify-content: space-between;
-      display: flex;
-      flex-direction: column;
-      align-items: flex-end;
-      font-size: 0.5rem;
-      padding: 0.5rem;
-    }
-  .menu{
-  border-color: white;
-    display: block;
-    top: 0.9rem;
-    right: 0.4rem;
-    z-index: 1;
-  
-  }
- 
-  .nav {
-      display: none;
-      flex-direction: column;
-      width: auto;
-      text-align: right;
-      font-size: 1rem;
-      position: absolute;
-      background-color: #3D5315;
-      top: 4.1rem;
-      right: 0rem;
-      left: 15rem;
-      z-index: 1000;
-     padding: 1rem;
-     border-radius: 5px;
-    
-      
-    }
-  .nav.nav-open{
-    display: flex;  
-  }
-    .nav ul {
-      flex-direction: column;
-      gap: 1rem;
-     padding: 0;
-     padding: 0;
-    }
-   .nav ul li {
-    border: 1px solid white;
-    padding: 0.5rem 1rem;
-    border-radius: 5px;
-      }
-    .web-intro {
-      font-size: 1.2rem;
-    }
-    .background-image {
-  position: relative;
-  margin-top: 1rem;
-  text-align: center;
-  height: 40vh;
-  overflow: hidden;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  }
-  .background-image img {
+
+  .web-footer-copyright {
     width: 100%;
-    height: 100%;
-    object-fit: cover;
-  }
-    .input-circle {
-      width: 220px;
-      height: 220px;
-      font-size: 1rem;
-    }
-    .logo {
-   margin-right: auto;
-  }
-  .web-logo img {
-    border: #82a73d;
-    border-radius: 20%;
-    width: 30px;
-    height: 30px;
-    margin-right: 0.5rem;
-    padding-left: 1.25%;
-  }
-  .NHS-links{
-    flex-direction: column;
-    align-items: center;
-    padding: 1rem;
-  }
-  .nhs-info-link{
-    width: 30%;
     text-align: center;
+    margin-top: 0.7rem;
   }
 
-.web-footer {
-  background-color: #3D5315; 
-  color: white; 
-  padding: 1rem;
-  text-align: center;
-  margin-top: 1rem; 
+
+  .web-footer p {
+    margin: 0;
+    font-size: 0.7rem;
   }
-.web-footer-copyright {
-  width: 100%;
-  text-align: center;
-  margin-top: 0.7rem;
-}
 
-
-.web-content {
-  display: flex;
-  justify-content: space-between;
-  align-items: center;
-  padding: 0 4rem;
-}
-.web-footer p {
-  margin: 0; 
-  font-size: 0.7rem;
-}
-
-  .web-footer-section{
+  .web-footer-section {
     font-size: 0.5rem;
 
   }
-  }
-  
-  @media (max-width: 480px) {
-    .web-header {
-      flex-direction: column;
-      align-items: center;
-      font-size: x-small;
-    }
-  
-    .nav {
-      justify-content: center;
-      padding-right: 0;
-    }
-  
-    .nav ul {
-      flex-direction: column;
-      gap: 1rem;
-      text-align: center;
-      font-size: medium;
-    }
-    .menu{
-    display: block;
-  }
-    .web-intro {
-      margin-top: -0.30rem;
-      font-size: 1rem;
-    }
-  
+}
 
-    .web-logo {
+@media (max-width: 760px) {
+  .web-header {
+    flex-direction: column;
+    align-items: center;
+    font-size: x-small;
+  }
+
+  .web-logo {
     display: flex;
     align-items: center;
   }
+
   .web-logo img {
     border: #82a73d;
     border-radius: 20%;
@@ -371,62 +210,90 @@ padding-left: 0.75%
     margin-right: 0.5rem;
     padding-left: 1.25%;
   }
-    .input-circle {
-      width: 220px;
-      height: 220px;
-      font-size: 0.8rem;
 
-    }
-    .background-image {
-  position: relative;
-  margin-top: 1rem;
-  text-align: center;
-  height: 40vh;
-  overflow: hidden;
-  display: flex;
-  justify-content: center;
-  align-items: center;
+  .nav {
+    display: none;
+    flex-direction: column;
+    align-items: center;
+    position: absolute;
+    top: 3rem;
+    right: 0rem;
+    background-color: #3D5315;
+    padding: 2rem;
+    border-radius: 5px;
+    z-index: 1;
+
   }
-  .background-image img {
+
+  .nav ul {
+    margin-top: 0rem;
+    ;
+    list-style: none;
+    display: flex;
+    flex-direction: column;
+    gap: 0.2rem;
+    text-align: center;
+
+  }
+
+  .nav a {
+
+    color: white;
+    text-decoration: none;
+    font-weight: bold;
+  }
+
+  .nav a:hover {
+    text-decoration: underline;
+
+  }
+
+  .menu {
+    background: none;
+    border: none;
+    color: white;
+    font-size: 1.5rem;
+    cursor: pointer;
+    display: block;
+    position: absolute;
+    right: 1rem;
+    margin-top: 0.5rem;
+
+  }
+
+  .nav.nav-open {
+    display: flex;
+  }
+
+  .web-footer {
+    background-color: #3D5315;
+    color: white;
+    padding: 1rem;
+    text-align: center;
+    margin-top: 0.75rem;
+  }
+
+  .web-footer-copyright {
     width: 100%;
-    height: 100%;
-  
+    text-align: center;
+    margin-top: 0.5rem;
   }
 
-    .nhs-info-link{
-      font-size: 1rem;
-    }
 
-.web-footer {
-  background-color: #3D5315; 
-  color: white; 
-  padding: 1rem;
-  text-align: center;
-  margin-top: 0.75rem; 
+  .web-footer-content {
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    padding: 0 3rem;
   }
-.web-footer-copyright {
-  width: 100%;
-  text-align: center;
-  margin-top: 0.5rem;
+
+  .web-footer p {
+    margin: 0;
+    font-size: 0.5rem;
+  }
+
+  .web-footer-section {
+    font-size: 0.5rem;
+  }
 }
-
-
-.web-footer-content {
-  display: flex;
-  justify-content: space-between;
-  align-items: center;
-  padding: 0 3rem;
-}
-.web-footer p {
-  margin: 0;
-  font-size: 0.5rem;
-}
-
-  .web-footer-section{
-    font-size: 0.3rem;
-  }
-  }
-  
- 
-  
-  </style>
+</style>
