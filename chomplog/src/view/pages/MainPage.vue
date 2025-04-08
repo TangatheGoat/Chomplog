070eccae4fda0e655f3c2ef0c5b0e8c1f1fdc223
Ayeshashoukat20 <21421627@stu.mmu.ac.uk>
2025-04-08 14:29:03 +0100

    Screen size changed

--- chomplog/src/view/pages/MainPage.vue
+++ chomplog/src/view/pages/MainPage.vue
@@ -133,7 +133,7 @@
    <style scoped>
    
 
-  @media (max-width: 2000px) {
+  @media screen {
     .web-intro {
     font-size: 1rem;
     color: #3D5315;
@@ -261,7 +261,7 @@ padding-left: 0.75%
   transform: scale(1.05);
 }
 .calorie-result {
-  margin-top: 1rem;
+  margin-top: 0.5rem;
   text-align: center;
   font-size: 0.9rem;
   color: #3D5315;
